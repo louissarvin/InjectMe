a86dc3391d29dbbe8efde29bb5a68dd0611b6a24
louissarvin <louisbaxel@gmail.com>
2026-04-30 10:30:00 +0700

    Add Farcaster frame provider

---
+++ web/src/providers/FarcasterProvider.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext } from 'react'
+
+// Farcaster is not applicable on 0G Galileo testnet.
+// This passthrough provider preserves existing import contracts.
+
+interface FarcasterContextValue {
+  isInMiniApp: boolean
+  context: null
+  isReady: boolean
+}
+
+const FarcasterContext = createContext<FarcasterContextValue>({
+  isInMiniApp: false,
+  context: null,
+  isReady: true,
+})
+
+export function FarcasterProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <FarcasterContext.Provider
+      value={{ isInMiniApp: false, context: null, isReady: true }}
+    >
+      {children}
+    </FarcasterContext.Provider>
+  )
+}
+
+export function useFarcasterContext() {
+  return useContext(FarcasterContext)
+}
