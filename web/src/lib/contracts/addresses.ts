294dd456ae0e7bcbc6f44f4d4eb136149ccd934e
louissarvin <louisbaxel@gmail.com>
2026-05-01 09:00:00 +0700

    Add contract address constants

---
+++ web/src/lib/contracts/addresses.ts
@@ -0,0 +1,13 @@
+import { config } from '@/config'
+
+export const CONTRACT_ADDRESSES = {
+  challengeFactory: config.contracts.challengeFactory,
+  agentNFT: config.contracts.agentNFT,
+  teeOracle: config.contracts.teeOracle,
+  reputationRegistry: config.contracts.reputationRegistry,
+  oracleStaking: config.contracts.oracleStaking,
+  challengeFactoryERC20: config.contracts.challengeFactoryERC20,
+  wrapped0GBase: config.contracts.wrapped0GBase,
+} as const
+
+export type ContractName = keyof typeof CONTRACT_ADDRESSES
